9f5adacad09c3a4d52ab57f01349a378aa62de69
puputea <113566259+puputea@users.noreply.github.com>
2021-07-03 07:19:06 +0000

    more migration

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -6,3 +6,10 @@ local _ = require("luassert.match")._
 
 describe("Lua state machine framework", function()
   describe("A stop light", function()
+    local fsm
+    local stoplight = {
+      { name = 'warn',  from = 'green',  to = 'yellow' },
+      { name = 'panic', from = 'yellow', to = 'red'    },
+      { name = 'calm',  from = 'red',    to = 'yellow' },
+      { name = 'clear', from = 'yellow', to = 'green'  }
+    }
