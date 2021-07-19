477514c1a1b6481043eecc130aff347e83c60a39
puputea <113566259+puputea@users.noreply.github.com>
2021-07-19 22:04:25 +0000

    v1.5.5

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -13,3 +13,13 @@ describe("Lua state machine framework", function()
       { name = 'calm',  from = 'red',    to = 'yellow' },
       { name = 'clear', from = 'yellow', to = 'green'  }
     }
+
+    before_each(function()
+      fsm = machine.create({ initial = 'green', events = stoplight })
+    end)
+
+    it("should start as green", function()
+      assert.are_equal(fsm.current, 'green')
+    end)
+
+    it("should not let you get to the wrong state", function()
