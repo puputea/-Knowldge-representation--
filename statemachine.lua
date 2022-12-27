7ad56a4c9dd6af2a888377e4a7e5b356ccc755a9
puputea <113566259+puputea@users.noreply.github.com>
2022-12-27 18:56:06 +0000

    t0302: check helper can handle empty credentials

--- statemachine.lua
+++ statemachine.lua
@@ -8,3 +8,12 @@ local ASYNC = "async"
 local function call_handler(handler, params)
   if handler then
     return handler(unpack(params))
+  end
+end
+
+local function create_transition(name)
+  local can, to, from, params
+
+  local function transition(self, ...)
+    if self.asyncState == NONE then
+      can, to = self:can(name)
